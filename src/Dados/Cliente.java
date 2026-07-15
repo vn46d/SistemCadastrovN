6559e77b0cbdd89cabba0fcd3a7a5bdcad4a17aa
vN46 <viniciussa171l@gmail.com>
2026-07-14 23:06:27 -0300

    assinando apenas.

--- src/Dados/Cliente.java
+++ src/Dados/Cliente.java
@@ -2,9 +2,42 @@ package Dados;
 
 public class Cliente {
 
-            Nome
-            CPF
-            Idade
+    private String nome;
+    private String cpf;
+    private int idade;
+    private String telefone;
 
-            Telefone
+
+
+        public String getNome() {
+        return nome;
+    }
+
+        public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+        public String getCpf() {
+        return cpf;
+    }
+
+        public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+        public int getIdade() {
+        return idade;
+    }
+
+        public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+        public String getTelefone() {
+        return telefone;
+    }
+
+        public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
--- src/Dados/Main.java
+++ src/Dados/Main.java
@@ -1,15 +1,68 @@
 package Dados;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner option = new Scanner(System.in);
+        ArrayList<Cliente> clientes = new ArrayList<>();
 
-        private String nome;
-        private String cpf;
-        private int idade;
-        private String telefone;
+    boolean executando = true;
+
+        while (executando) {
+
+            Menu.mostrarMenu();
+
+            String escolha = option.nextLine();
+
+            switch (escolha) {
+
+                case "1":
+
+                    Cliente cliente = new Cliente();
+
+                    System.out.print("Digite o nome: ");
+                    cliente.setNome(option.nextLine());
+
+                    System.out.print("Digite o CPF: ");
+                    cliente.setCpf(option.nextLine());
+
+                    System.out.print("Digite a idade: ");
+                    cliente.setIdade(option.nextInt());
+                    option.nextLine();
+
+                    System.out.print("Digite o telefone: ");
+                    cliente.setTelefone(option.nextLine());
+
+                    clientes.add(cliente);
+
+                    System.out.println("Cliente cadastrado!");
+                    break;
+
+                case "2":
+
+                    for (Cliente c : clientes) {
+
+                        System.out.println("Nome: " + c.getNome());
+                        System.out.println("CPF: " + c.getCpf());
+                        System.out.println("Idade: " + c.getIdade());
+                        System.out.println("Telefone: " + c.getTelefone());
+                        System.out.println();
+                    }
+                    break;
 
 
+                case "6":
+                    executando = false;
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
     }
 }
